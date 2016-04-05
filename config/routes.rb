d9fff258965863fb3bd07fecb8aa01564822a960
sandjiv <sandjiv.parassou@gmail.com>
2016-04-05 15:12:19 +0200

    end of chapter 8, still one failed test though

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'sessions/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -18,7 +20,14 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
 
+  get    'login'   => 'sessions#new'
+
+  post   'login'   => 'sessions#create'
+
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
