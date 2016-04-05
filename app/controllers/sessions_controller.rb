8cf01c5e90f0da1173c2a8d80bf30e4b7165aa6d
sandjiv <sandjiv.parassou@gmail.com>
2016-04-05 18:40:29 +0200

    end of chapter 9, destroying accounts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,11 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in(user)
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
-      redirect_to user #equivalent à user_url(user)
+      if session[:forwarding_url]
+        redirect_to session[:forwarding_url]
+      else
+        redirect_to user #equivalent à user_url(user)
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:name,presence: true, length: { maximum: 50})
   validates(:email,presence: true, length: { maximum: 255}, format: { with:VALID_EMAIL_REGEX},uniqueness:{case_sensitive:false})
-  validates(:password,presence: true, length: {minimum: 6})
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
   has_secure_password
-
-  # Returns the hash digest of the given string.
+    # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
@@ -34,4 +33,5 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+
 end
